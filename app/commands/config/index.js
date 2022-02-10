8534aa62c3f7e850e174c40ec0f9322dc7728f19
ayyu <ayyu@uwaterloo.ca>
2022-02-10 12:43:14 -0500

    removed old code from template

--- app/commands/config/index.js
+++ app/commands/config/index.js
@@ -1,5 +1,4 @@
 module.exports = keyvs => [
 	require('./channel')(keyvs),
 	require('./role')(),
-	require('./icon')(),
 ];
--- app/utilities/register-commands.js
+++ app/utilities/register-commands.js
@@ -5,15 +5,15 @@ const { Routes } = require('discord-api-types/v9');
  * @typedef {import('discord.js').OAuth2Guild} Guild
  */
 
+const commands = require('../commands')().map(command => command.data.toJSON());
+
 /**
  * @param {Client} client
  * @param {string} token
  */
 const registerCommands = async (client, token = null) => {
 	token = token ?? client.token;
-	const commands = require('../commands')().map(command => command.data.toJSON());
 	const rest = new REST({ version: '9' }).setToken(token);
-
 	return rest.put(Routes.applicationCommands(client.user.id), { body: commands })
 		.then(() => console.log('Successfully registered global application commands.'))
 		.catch(console.error);
@@ -23,11 +23,9 @@ const registerCommands = async (client, token = null) => {
  * @param {Client} client
  * @param {string} token
  */
- const registerGuildCommands = async (client, guildId, token = null) => {
+const registerGuildCommands = async (client, guildId, token = null) => {
 	token = token ?? client.token;
-	const commands = require('../commands')().map(command => command.data.toJSON());
 	const rest = new REST({ version: '9' }).setToken(token);
-
 	return rest.put(Routes.applicationGuildCommands(client.user.id, guildId), { body: commands })
 		.then(() => console.log(`Successfully registered application commands for guild ${guildId}.`))
 		.catch(console.error);
